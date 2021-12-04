c6addea7574d11873bc4ebc53c210ff16812ffb9
StartMeUp <contact@start-me-up.fr>
2021-12-04 11:55:59 +0100

    header and footer in router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+//import pages
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 import NotFound404 from "./pages/NotFound404";
+
+//import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div id="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movie/:id" element={<Movie />} />
           <Route path="*" element={<NotFound404 />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
