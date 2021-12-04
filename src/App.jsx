054c5c6223040521ec9d5e523e97b0f9e356f2ca
StartMeUp <contact@start-me-up.fr>
2021-12-04 11:40:13 +0100

    router ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import NotFound404 from "./pages/NotFound404";
 
 const App = () => {
-  return <h1>This is my App</h1>;
+  return (
+    <div id="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="*" element={<NotFound404 />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
